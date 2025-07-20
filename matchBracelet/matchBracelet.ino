0a4f06169bb02d3d1b8551eef124e025b4061be2
Samuel Tapia <samfertap@gmail.com>
2025-07-19 22:19:34 -0600

    Added a few functions

--- matchBracelet/matchBracelet.ino
+++ matchBracelet/matchBracelet.ino
@@ -1,7 +1,47 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+/**
 
+
+
+1 Entrada PullDown (Espada)
+1 Entrada PullUp (Florete)
+2 Entrada GPIO
+
+1 Entrada (deviceMode)
+1 sntrada GPIO
+
+5 Salidas GPIO
+3 Entrada GPIO
+8 PINES GPIO
+
+**/
+
+#define whiteLed1      3
+#define whiteLed2      4
+#define blueLed1       10
+#define blueLed2       1
+#define buzzer         0
+#define espadaPullDown 5
+#define floretePullUp  6
+#define deviceMode           2 // Usar con pull-down físico
+
+//Functions
+void notifyOk();
+
+//Variables
+enum Mode{
+  Fencing,
+  Riposte,
+  Point
+}currentMode;
+
+//Struct to be sent
+
+typedef struct StructMessage{
+  bool status;
+} structMessage;
 
 //Address, hexadecimal {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
 uint8_t broadcastAddress[] ={};
@@ -18,13 +58,31 @@ void onDataRecv(const uint8_t *mac,const uint8_t *incomingData, int len){
 }
 
 void setup() {
+
+  //Pin configuration
+  pinMode(whiteLed1, OUTPUT);
+  pinMode(whiteLed2, OUTPUT);
+  pinMode(blueLed1, OUTPUT);
+  pinMode(blueLed2, OUTPUT);
+  pinMode(buzzer,OUTPUT);
+
+  pinMode(espadaPullDown,INPUT_PULLDOWN);
+  pinMode(floretePullUp,INPUT_PULLUP);
+  pinMode(deviceMode,INPUT_PULLDOWN);
   
   //Esp Now communication protocol
   WiFi.mode(WIFI_STA);
   if(esp_now_init() != ESP_OK){
     Serial.println("Error initializing");
+    digitalWrite(buzzer,HIGH);
+    delay(500);
+    digitalWrite(buzzer,LOW); 
     return;
   }
+
+  notifyOk();
+  //When its succesfully connected, we turn on leds.
+  
   //Register for send cb to get status of transmitted packet
   esp_now_register_send_cb(onDataSent);
 
@@ -44,10 +102,38 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  switch(currentMode){
+    case Mode::Fencing:
+      break;
+    case Mode::Point:
+      break;
+    case Mode::Riposte:
+      break;
+  }
+
+
+  
   //TO SEND DATA USE
   //esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&struct, sizeof(struct));
   //if(result==ESP_OK){...} else{...}
 
+}
+
+void notifyOk(){
+  digitalWrite(whiteLed1,HIGH);
+  digitalWrite(whiteLed2,HIGH);
+  digitalWrite(blueLed1,HIGH);
+  digitalWrite(blueLed2,HIGH);
+  digitalWrite(buzzer,HIGH);
+  delay(400);
+  digitalWrite(buzzer,LOW);
+  delay(200);
+  digitalWrite(buzzer,HIGH);
+  delay(200);
+  digitalWrite(whiteLed1,LOW);
+  digitalWrite(whiteLed2,LOW);
+  digitalWrite(blueLed1,LOW);
+  digitalWrite(blueLed2,LOW);
+  digitalWrite(buzzer,LOW);
 }
